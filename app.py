42598cddaf8972d5eeaebb47a289218ea72026ab
SeifEhab <95101416+seifehab33@users.noreply.github.com>
2024-06-10 18:35:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def index():
 if __name__ == "__main__":
     try:
         start_cameras()
-        # app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
-        app.run(host='0.0.0.0',port=5000, debug=True, threaded=True)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
     finally:
         stop_cameras()
